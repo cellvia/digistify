a5e2bb6658aa0e55bad8f971465c69418feacf00
Brandon Selway <cellvia@yahoo.com>
2014-04-23 22:33:04 -0600

    normalize returned data, better etag support

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var exportObj = {
 			exportObj.defaults[prop] = val;		
 	},
 	getGists: function (user, options, cb){
-		if(!isNaN(user)) return exportObj.getContent.apply(exportObj, arguments);
+		if(!isNaN(user)) return exportObj.getFiles.apply(exportObj, arguments);
 		if(typeof options === "function"){
 			cb = options;
 			options = {};
@@ -37,21 +37,23 @@ var exportObj = {
 		}
 
 		var opts = {json: true, headers: {} };
+		if(options.etag){
+			opts.headers['If-None-Match'] = options.etag;
+			delete options.etag; 
+		}
 		if(typeof options.headers === "object") merge(opts.headers, options.headers);
 		if(typeof exportObj.defaults.headers === "object") merge(opts.headers, exportObj.defaults.headers);
 		if(!process.browser && !opts.headers['User-Agent']) opts.headers['User-Agent'] = options.userAgent || 'node.js';
 
-		function finalize(){
-			if(offset || limit) gists = gists.slice(offset, limit);
-			if( transform ) gists = gists.map(transform);
-			cb(null, gists);
-		}
+		var etag;
 
 		function recurs(){
 			opts.url = get_gists_url(user, {per_page: 100, page: ++counter});
 			request( opts, function(err, resp, newgists){
+				if(resp.statusCode == 304) return cb(null, "unmodified");
 				if(err) return cb(err);
 				if(resp.statusCode != 200) return cb(new Error("invalid url: "+ opts.url));
+				if(!etag) etag = resp.headers ? resp.headers['ETag'] : resp.getResponseHeader('ETag');
 				if(typeof newgists !== "object" || typeof newgists.length === "undefined" || newgists.message) return cb(new Error("error: "+ newgists));
 				if(!newgists.length) return finalize();
 
@@ -82,9 +84,15 @@ var exportObj = {
 			});
 		}
 		recurs();
+
+		function finalize(){
+			if(offset || limit) gists = gists.slice(offset, limit);
+			if( transform ) gists = gists.map(transform);
+			cb(null, { data: gists, etag: etag });
+		}
 	},
 
-	getContent: function(id, options, cb){
+	getFiles: function(id, options, cb){
 		if(typeof options === "function"){
 			cb = options;
 			options = {};
@@ -93,35 +101,37 @@ var exportObj = {
 		var contents = [];
 		var transform;
 
-		switch(options.contentTransform){
+		switch(options.fileTransform){
 			case "article":
 				transform = function(file){
 					return file.content
 				}
 			break;
 			default:
-				transform = typeof options.contentTransform === "function" ? options.contentTransform : false;
+				transform = typeof options.fileTransform === "function" ? options.fileTransform : false;
 			break;
 		}
 
 		var opts = {json: true, headers: {}, url: get_gist_url(id) };
+		if(options.etag){
+			opts.headers['If-None-Match'] = options.etag;
+			delete options.etag; 
+		}
 		if(typeof options.headers === "object") merge(opts.headers, options.headers);
 		if(typeof exportObj.defaults.headers === "object") merge(opts.headers, exportObj.defaults.headers);
 		if(!process.browser && !opts.headers['User-Agent']) opts.headers['User-Agent'] = options.userAgent || 'node.js';
 
 		request( opts, function(err, resp, gist){
+			if(resp.statusCode == 304) return cb(null, "unmodified");
 			if(err) return cb(err);
 			if(resp.statusCode != 200) return cb(new Error("invalid url? "+ opts.url));
+			var etag = resp.headers ? resp.headers['ETag'] : resp.getResponseHeader('ETag');
 			for(var file in gist.files){
 				if(options.language && gist.files[file].language.toLowerCase() !== options.language.toLowerCase()) continue
 				contents.push(gist.files[file]);
 			}
 			if(transform) contents = contents.map(transform);
-			if(contents.length === 1){
-				cb(null, contents[0]);
-			}else{
-				cb(null, contents);
-			}
+			cb(null, { data: contents, etag: etag });
 		});
 	}
 }
